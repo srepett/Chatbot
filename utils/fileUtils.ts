96a8d152083aca7fa89accae6840151cfdac18f5
Zymzz <srepettsrepett85@gmail.com>
2025-10-30 07:02:26 +0800

    Add files via upload

---
+++ utils/fileUtils.ts
@@ -0,0 +1,24 @@
+
+// Converts a File object to a GoogleGenerativeAI.Part object.
+export const fileToGenerativePart = async (file: File) => {
+  const base64EncodedDataPromise = new Promise<string>((resolve) => {
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      if (typeof reader.result === 'string') {
+        resolve(reader.result.split(',')[1]);
+      } else {
+        resolve(''); // or handle ArrayBuffer case if necessary
+      }
+    };
+    reader.readAsDataURL(file);
+  });
+
+  const base64EncodedData = await base64EncodedDataPromise;
+
+  return {
+    inlineData: {
+      data: base64EncodedData,
+      mimeType: file.type,
+    },
+  };
+};
